7c0fc6b50a99fbab876486740e33eeba1096498c
sehaj97 <sehajmagan@gmail.com>
2022-03-07 20:10:02 -0500

    added clean code

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -15,14 +15,14 @@ function Cards ({books, numFound}){
         )}
             { books && (<div className="row" > 
                 {books.map((book, index) => (
-                <div className="column" > 
+                <div className="column" key={book.title + index}> 
                         <div className="card" > 
                          <h2> {book.title}</h2>
                          <img
                             src={book.isbn?`https://covers.openlibrary.org/b/isbn/${book.isbn[0]}-L.jpg`: require(`../../assets/images/nobook.jpg`)}
                             alt={book.title}
                             className="img-fit"
-                            key={book.isbn?book.isbn: index}
+                            key={book.title + index}
                         />
                             {book.author_name && (<h3>By: {book.author_name[0]}</h3>)}
                             {book.first_publish_year && (<p>Published On: {book.first_publish_year}</p>)}
