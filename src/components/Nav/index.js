35df768cfca4605f6ebd141235a13913b4f4670a
sehaj97 <sehajmagan@gmail.com>
2022-03-07 19:38:10 -0500

    added sort functionality

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Nav() {
     return (
         <div className="topnav">
-            <a className="title" href="#home">Open Library UI App</a>
+            <a className="title" href="#">BrowseBook</a>
         </div>
     );
 }
